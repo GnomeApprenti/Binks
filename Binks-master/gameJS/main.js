845a01ad009329552d1633a792b2b55babc280cb
GnomeApprenti <42942956+GnomeApprenti@users.noreply.github.com>
2020-02-22 19:11:25 +0100

    Ajout déplacement claver
    -zqsd et flèches
    bugs :
    -impossible de faire 2 mouvements à la fois

--- Binks-master/gameJS/main.js
+++ Binks-master/gameJS/main.js
@@ -5,12 +5,10 @@ const ctx = canvas.getContext("2d");
 const HEIGHT = 750;
 const WIDTH = 1500;
 
-setInterval(update,40);
-
 //constructor
-const listeJoueurs = {};
+let listeJoueurs = {};
 
-function Entity(nom, x, y, sizeX, sizeY, id){
+Entity = function (nom, x, y, sizeX, sizeY, vitX, vitY, id){
 
   let joueur = {
     x:x,
@@ -18,27 +16,83 @@ function Entity(nom, x, y, sizeX, sizeY, id){
     sizeX:sizeX,
     sizeY:sizeY,
     nom:nom,
+    vitX:vitX,
+    vitY:vitY,
     id:id,
   };
   listeJoueurs[id] = joueur;
 
 }
 
-Entity('booba', 150, 150, 100, 200, 'idBooba');
-Entity('kaaris', 350, 150, 100, 200, 'idKaaris');
-
 //affichage des entités
-function updateEntity(entity){
 
-  ctx.fillRect(entity.x, entity.y, entity.sizeX, entity.sizeY);
+DrawEntity = function (entity){
+
+    ctx.fillRect(entity.x, entity.y, entity.sizeX, entity.sizeY);
 
 }
 
-function update(){
+UpdateEntity = function (entity){
+
+  DrawEntity(entity);
+
+}
+
+Update = function (){
 
   ctx.clearRect(0,0,WIDTH,HEIGHT);
   for (var i in listeJoueurs){
-    updateEntity(listeJoueurs[i]);
+    UpdateEntity(listeJoueurs[i]);
   }
 
 }
+
+Start = function(){
+
+  Entity('booba', 150, 150, 100, 200, 5, 5, 'idBooba'); //(nom, x, y, sizeX, sizeY, vitX, vitY, id)
+  Entity('kaaris', 350, 150, 100, 200, 7, 7, 'idKaaris');
+
+}
+
+document.onkeydown = function(event){
+
+  switch(event.keyCode){
+    case 38:
+      //haut
+      listeJoueurs['idBooba'].y -= listeJoueurs['idBooba'].vitY
+      break;
+    case 37:
+      //gauche
+      listeJoueurs['idBooba'].x -= listeJoueurs['idBooba'].vitY
+      break;
+    case 39:
+      //droite
+      listeJoueurs['idBooba'].x += listeJoueurs['idBooba'].vitY
+      break;
+    case 40:
+      //bas
+      listeJoueurs['idBooba'].y += listeJoueurs['idBooba'].vitY
+      break;
+    case 90:
+      //haut
+      listeJoueurs['idKaaris'].y -= listeJoueurs['idKaaris'].vitY
+      break;
+    case 81:
+      //gauche
+      listeJoueurs['idKaaris'].x -= listeJoueurs['idKaaris'].vitY
+      break;
+    case 68:
+      //droite
+      listeJoueurs['idKaaris'].x += listeJoueurs['idKaaris'].vitY
+      break;
+    case 83:
+      //bas
+      listeJoueurs['idKaaris'].y += listeJoueurs['idKaaris'].vitY
+      break;
+
+  }
+
+}
+
+setInterval(Update,40);
+Start();
